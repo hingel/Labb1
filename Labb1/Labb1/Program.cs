a883ac706355a0a1b3517bfe979b253dfe223ada
hingel <henrik.ingelsten@iths.se>
2022-09-06 13:17:41 +0200

    Update

--- Labb1/Labb1/Program.cs
+++ Labb1/Labb1/Program.cs
@@ -2,51 +2,74 @@
 
 //string input = Console.ReadLine();
 
-input = "29535123p48723487597645723645";
+string input = "29535123p48723487597645723645";
 char searchChar;
 
-int totalSum = 0;
-int sum = 0;
+long totalSum = 0;
+long sum = 0;
 
 string firstString = string.Empty;
 string numberString = string.Empty; //som ska summeras och färgas
 string secondString = string.Empty;
-bool falseString = false;
-
+bool checkConv = false; //om konvertering gått igenom
+bool lastPart= false; //Om sista delen ska fyllas i
 
 //Loop för att gå igenom alla indexering av strängen
 for (int i = 0; i < input.Length; i++)
 {
     searchChar = input[i]; //Vad som ska letas efter
-    firstString += input[i]; //den första delen av strängen.
-
-    if (falseString) //om true ska strängen nollställas.
+    numberString += input[i];
+    
+    for (int j = i + 1; j < input.Length; j++)
     {
-        numberString = string.Empty;
-    }
-
-    for (int j = 0; j < input.Length; j++)
-    {
-        numberString += input[j]; //lägger till 
-
-        if (input[j] == searchChar) //då har nästa likadana värde hittats
+        if (!lastPart)  //
         {
-            falseString = int.TryParse(numberString, out sum); //Kör try parse här om det går att konvertera texten.
+            numberString += input[j]; //lägger till delar till siffersträngen
+        }
 
-            if (falseString)
+        //Här måste den sista sträng delen göras, om vi fått ok på variabeln nedan. Placeras ovan checken nedan för att få utskriften ok.
+        if (lastPart)
+        {
+            secondString += input[j];
+        }
+
+        if (input[j] == searchChar && lastPart == false) //då har nästa likadana värde hittats, inte samma som första siffran
+        {            
+            checkConv = long.TryParse(numberString, out sum); //Kör try parse här om det går att konvertera texten.
+
+            if (checkConv) //om konvertering gick igenom
             {
                 totalSum += sum; //lägg sum till totalsumman
-                //sen ska vi fortsätta att lägga till 
+                lastPart = true; //Då börja på sista strängen
             }
 
-            else
+            else if (!checkConv) //Om konverteringen ej går igenom
             {
-                //Kanske borde lägga nollställningen av nummersträngen här?
+                numberString = string.Empty;
+                //och sen börja om från början för nästa i position. i position med ett nytt sökvärde
                 break;
             }
-
-            //om det går har vi ett korrekt värde och sista delen av strängen kan letas upp.
-            //annars om det ej är korrekt måste vi börja om på i = 1 och tömma nummer strängen igen.
         }
     }
+
+    if (checkConv) //Om konvertering blivit lyckad vill vi skriva ut.
+    {
+        lastPart = false;
+        //Här nere ska strängen skrivas ut
+        Console.WriteLine("{0},{1},{2} i: {3} searchChar: {4}", firstString, numberString, secondString, i, searchChar);
+        numberString = string.Empty;
+        secondString = string.Empty;
+        checkConv = false;
+        firstString += input[i]; //Lägga till sist för att inte komma med i utskriften, men ska alltid summeras.
+    }
+
+    else
+    {
+        numberString = string.Empty;
+        secondString = string.Empty;
+        lastPart = false;
+        firstString += input[i]; //summera ihop strängen.
+    }
 }
+
+Console.WriteLine("Summan är: " + totalSum);
